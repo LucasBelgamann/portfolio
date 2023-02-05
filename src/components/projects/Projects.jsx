1a6c44b55592b9508dea1830002e99e89bcbf8dd
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-02-05 14:22:44 -0300

    feat: projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,20 +3,30 @@ import "./Projects.css";
 
 const Projects = () => {
   return (
-    <div>
-      <div className="project-content">
-        <div className="project-title">
-        <h4>Conheça alguns de meus</h4>
-        <h3>Projetos</h3>
+    <div className="cont-project">
+      <h2 className="title-projects">Projetos</h2>
+      <div className="container-projects">
+      {Data.map((e) => (
+        <div class="card">
+          <div class="circle"></div>
+          <div class="content">
+            <h2>{e.title}</h2>
+            <p>{e.description}</p>
+            <div className="techs">
+              {
+                e.techs.map((tech) => <img src={tech.img} alt={e.title} />)
+              }
+            </div>
+            <button type="button" className="more">
+              <a href={e.link} target="_blank" rel="noreferrer">
+                Ver mais...
+              </a>
+            </button>
+          </div>
+          <img className="image" src={e.img} alt="" />
         </div>
-        <div className="projects">
-          {Data.map((e) => (
-            <a href={e.link} target="_blank" rel="noreferrer">
-              <img src={e.img} alt="hard" />
-            </a>
-          ))}
-        </div>
-      </div>
+      ))}
+    </div>
     </div>
   );
 };
--- src/components/projects/ProjectsData.js
+++ src/components/projects/ProjectsData.js
@@ -1,23 +1,90 @@
 import jhon from './img/1.jpg';
+import javaScript from './images/javaScript.png';
+import nodejs from './images/node-js.png';
+import react from './images/react.png';
+import mySql from './images/mySql.png';
+import css from './images/css.png';
 import acqualine from './img/acqualine.jpg';
 import tfc from './img/tfc.jpg';
 import recipesApp from './img/recipesApp.jpg';
+import typesS from './images/typesS.png';
 
 export const Data = [
     {
+        title: 'Delivery app',
         img: jhon,
-        link: 'https://github.com/LucasBelgamann/delivery_app'
+        link: 'https://github.com/LucasBelgamann/delivery_app',
+        description: 'Nessa aplicação, o grupo foi responsável por criar e integrar tanto o back-end quanto o front-end, sendo o principal objetivo desenvolver um app de delivery para uma distribuidora de bebidas.',
+        techs: [
+            {
+                img: javaScript
+            },
+            {
+                img: nodejs
+            },
+            {
+                img: mySql
+            },
+            {
+                img: css
+            },
+            {
+                img: react
+            }
+        ]
     },
     {
-        img: acqualine,
-        link: 'https://acqualine.vercel.app/'
-    },
-    {
+        title: 'App de receitas',
         img: recipesApp,
-        link: 'https://recipes-app-project-umber.vercel.app/'
+        link: 'https://recipes-app-project-umber.vercel.app/',
+        description: 'O app de receitas é um projeto onde foi contruido um aplicativo de receitas, que no qual o usuário tem a possibilidade de de favoritar, salvar, compartilhar uma receita. Tendo acesso a uma página completa com todos os detalhes de como fazer o prato ou uma bebida.',
+        techs: [
+            {
+                img: javaScript
+            },
+            {
+                img: nodejs
+            },
+            {
+                img: mySql
+            },
+            {
+                img: css
+            },
+            {
+                img: react
+            }
+        ]
     },
     {
+        title: 'Futebol clube',
         img: tfc,
-        link: 'https://github.com/LucasBelgamann/futebol-clube'
+        link: 'https://github.com/LucasBelgamann/futebol-clube',
+        description: 'Nesse projeto foi contruido um back-end/banco de dados para que a API fosse capaz de ser consumida pelo front-end da aplicação.',
+        techs: [
+            {
+                img: typesS
+            },
+            {
+                img: nodejs
+            },
+            {
+                img: mySql
+            },
+        ]
+    },
+    {
+        title: 'Acqualine',
+        img: acqualine,
+        link: 'https://acqualine.vercel.app/',
+        description: 'Nesse projeto, foi contruido uma interface para o usuário onde o principal obejtivo é a divulgação dos serviços e produtos da loja.',
+        techs: [
+            {
+                img: react
+            },
+            {
+                img: css
+            },
+        ]
     },
 ]
