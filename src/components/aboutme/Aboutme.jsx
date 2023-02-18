98125944cd9cc83199833c316f3fca174180fac8
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-02-18 12:26:38 -0300

    fix: about text

--- src/components/aboutme/Aboutme.jsx
+++ src/components/aboutme/Aboutme.jsx
@@ -6,7 +6,7 @@ const Aboutme = () => {
         <div className="aboutme">
                 <h2>Um cara apaixonado por tecnologia e inovação!</h2>
             <div className='line-about'></div>
-                <p>Olá! Meu nome é Lucas e sou um desenvolvedor afixionado por solucionar problemas e criar soluções inovadoras. Acredito que a tecnologia é uma das coisas pode mudar o mundo e sempre busco maneiras de contribuir para esse objetivo.</p>
+                <p>Olá! Meu nome é Lucas e sou um desenvolvedor afixionado por solucionar problemas e criar soluções inovadoras. Acredito que a tecnologia é uma das coisas que pode mudar o mundo e sempre busco maneiras de contribuir para esse objetivo.</p>
         </div>
     </div>
   );
